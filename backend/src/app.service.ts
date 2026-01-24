465284caf6f6330e5bab4aff36757c2da340f924
shichenitu <shichenitu@163.com>
2026-01-24 12:19:45 +0100

    introduce toolbox: ConfigService & OpenAI

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config'; // to read the password of .env
+import OpenAI from 'openai';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  private openai: OpenAI; // private variable: for storing the OpenAI instance
+
+  constructor(private configService: ConfigService){
+    this.openai = new OpenAI({
+      apiKey: this.configService.get<string>('OPENAI_API_KEY') // apiKey: to be added
+    })
   }
+
+
 }
