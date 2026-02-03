33a7f7e5234ed69c59274cd265cc78efe4453ef0
shichenitu <shichenitu@163.com>
2026-02-03 23:42:59 +0100

    feat: implement signals for loading state and error handling

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,44 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('frontend');
+  // core three states
+  selectedFile = signal<File | null>(null);
+  caption = signal<string>('');
+  isLoading = signal<boolean>(false);
+
+  // inject HttpClient
+  constructor(private http: HttpClient) {}
+
+  onFileSelected(event: any) {
+    this.selectedFile.set(event.target.files[0]);
+  }
+
+  onUpload() {
+    const file = this.selectedFile();
+    if (!file) return;
+
+    this.isLoading.set(true); // loading
+    
+    const formData = new FormData();
+    formData.append('file', file);
+
+    // send the request
+    this.http.post<{description: string}>('http://localhost:3000/upload', formData)
+      .subscribe({
+        next: (res) => {
+          this.caption.set(res.description);
+          this.isLoading.set(false); // finish loading
+        },
+        error: () => {
+          alert('Failed upload');
+          this.isLoading.set(false);
+        }
+      });
+  }
 }
