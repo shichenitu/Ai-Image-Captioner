41f06b6df5f6909b0e99aa9a2ebaab0566f891a8
shichenitu <shichenitu@163.com>
2026-02-04 23:30:01 +0100

    solve the problem of CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: '*', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  console.log(`🚀 Backend is running on: http://localhost:${port}`);
 }
 bootstrap();
